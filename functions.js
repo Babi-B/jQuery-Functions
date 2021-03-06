d086ba7af0aa0fcc79275e5a83642b65a4c1e875
Babi-B <babibyoolah@gmail.com>
2021-03-06 02:03:06 +0100

    added work on fadeToggle and slideToggle functions

--- functions.js
+++ functions.js
@@ -9,6 +9,22 @@ $(document).ready(function(){
         $('.toggleClass').toggleClass("colorChange");
       });
 
-     
+     //fadeToggle() Function
+     $('button#fader').click(function(){
+        $('#fadeToggle1').fadeToggle()
+        $('#fadeToggle2').fadeToggle('slow')
+         $('#fadeToggle3').fadeToggle(3000)
+     })
+
+     //slideToggle() Function
+     $('#slideToggle1').click(function(){
+         $('#panel1').slideToggle()
+     })
+     $('#slideToggle2').click(function(){
+        $('#panel2').slideToggle('slow')
+    })
+    $('#slideToggle3').click(function(){
+        $('#panel3').slideToggle(3000)
+    })
 })
 
